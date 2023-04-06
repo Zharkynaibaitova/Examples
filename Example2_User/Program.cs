0663be80c23cff078866f47ae95973d7f46c3a02
Zharkyn <zharkynaibaitova@gmail.com>
2023-04-06 21:44:18 +0600

    Example2_User

---
+++ Example2_User/Program.cs
@@ -0,0 +1,7 @@
+﻿//string username = Console.Readline();
+// Console.Writeline("Привет, ");
+// Console.Writeline(username);
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
